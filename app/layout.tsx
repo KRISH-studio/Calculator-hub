89b6e1a6b95cc0d7dd38ac85c28cba74a7436634
KRISH-studio <krishsinghal012@gmail.com>
2025-05-21 12:42:51 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,8 @@ export default function RootLayout({
             `,
           }}
         />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5670862530868386"
+           crossorigin="anonymous"></script>
          <meta name="google-adsense-account" content="ca-pub-5670862530868386"/> 
         <meta name="google-site-verification" content="ad-qcNq_GrrumhwGtiGccvsvrFy2TsdRtVJyqWBiKZk" />
         <link rel="shortcut icon" href="calculator.png" type="image/x-icon" />
